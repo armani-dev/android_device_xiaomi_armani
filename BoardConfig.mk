a419bb76cdc41e5732249549a25f0b5d8782cfe5
fefifofum <jorsellis@gmail.com>
2015-03-01 20:39:48 +0100

    Add GPS HAL
    from CAF tag LNX.LA.3.5.2.2.2-00510-8x26.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,10 +80,6 @@ BOARD_FLASH_BLOCK_SIZE             := 131072
 # FM
 BOARD_HAVE_QCOM_FM := true
 
-# GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
-TARGET_NO_RPC := true
-
 # Graphics
 BOARD_EGL_CFG := device/xiaomi/armani/configs/egl.cfg
 TARGET_QCOM_DISPLAY_VARIANT := caf-new
